6ec92c8703de438f185fc283e0599d289a98dc36
zwirok-git <oleksijprokopenko@gmail.com>
2026-04-12 13:05:21 +0300

    Defined errors

---
+++ app/errors.py
@@ -0,0 +1,16 @@
+class VaccineError(Exception):
+    pass
+
+
+class NotVaccinatedError(VaccineError):
+    pass
+
+
+class OutdatedVaccineError(VaccineError):
+    pass
+
+
+class NotWearingMaskError(VaccineError):
+    pass
+
+
